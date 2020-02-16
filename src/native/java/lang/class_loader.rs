d07c9a85d90781c544ceec24aee2e3ea6f01fd3f
comradeHsu <61239289@qq.com>
2020-02-16 20:22:47 +0800

    impl CLassLoader.findLoadClass0 function

--- src/native/java/lang/class_loader.rs
+++ src/native/java/lang/class_loader.rs
@@ -15,6 +15,12 @@ pub fn init() {
         "(Ljava/lang/String;Z)V",
         load,
     );
+    Registry::register(
+        "java/lang/ClassLoader",
+        "findLoadedClass0",
+        "(Ljava/lang/String;)Ljava/lang/Class;",
+        find_loaded_class0,
+    );
 }
 
 /// public static native String findBuiltinLib(String name);
@@ -30,4 +36,19 @@ pub fn find_built_in_lib(frame: &mut Frame) {
 pub fn load(frame: &mut Frame) {
     let name = frame.local_vars().expect("vars is none").get_ref(1);
     println!("lib name:{}", java_str_to_rust_str(name.clone().unwrap()));
+}
+
+/// private native final Class<?> findLoadedClass0(String name);
+/// (Ljava/lang/String;)Ljava/lang/Class;
+pub fn find_loaded_class0(frame: &mut Frame) {
+    let class = frame.method().class();
+    let loader = (*class).borrow().loader();
+    let name = frame.local_vars().expect("vars is none").get_ref(1);
+    let class_name = java_str_to_rust_str(name.unwrap());
+    let class = (*loader).borrow().get_class(class_name.as_str());
+    if class.is_none() {
+        println!("None CLass Is {}",class_name)
+    }
+    let java_class = (*class.unwrap()).borrow().get_java_class();
+    frame.operand_stack().expect("stack is none").push_ref(java_class);
 }
